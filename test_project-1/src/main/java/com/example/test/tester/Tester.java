65c4098895180989253af5b2520ced19e40d76bd
Mamy <xingyanjintian@gmail.com>
2025-02-03 15:13:08 +0900

    余分なパッケージを削除し正常に動くよう動作を修正

--- test_project-1/src/main/java/com/example/test/tester/Tester.java
+++ test_project-1/src/main/java/com/example/test/tester/Tester.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 import com.example.test.test_code.Test_Code;
 
 public class Tester {
-Test_Code test = new Test_Code();
-@Test
-void addTest() {
-	int result = test.add(10, 20);
-	assertEquals(30,result);
-}
+	Test_Code test = new Test_Code();
+
+	@Test
+	void addTest() {
+		int result = test.add(10, 20);
+		assertEquals(30, result);
+	}
 }
