4fe84c05be7b41786e5ffbf826ba4c2d7e09cb03
Mamy <xingyanjintian@gmail.com>
2025-02-04 11:04:19 +0900

    動作確認用にspringを介さずにフォルダー作成クラスが動作できるように修正

--- test_project-1/src/main/java/com/example/test/TestProject1Application.java
+++ test_project-1/src/main/java/com/example/test/TestProject1Application.java
@@ -1,13 +1,18 @@
 package com.example.test;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.test.test_code.CreateFolder;
 
 @SpringBootApplication
 public class TestProject1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestProject1Application.class, args);
+	//	SpringApplication.run(TestProject1Application.class, args);
+		CreateFolder createFolder = new CreateFolder();
+		createFolder.foldeWriter("tester");
+		createFolder.watchFolder();
 	}
 
 }
